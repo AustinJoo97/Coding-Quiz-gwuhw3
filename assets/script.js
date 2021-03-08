8fa548c9395f9c117a847df3ad90488fe7677a1c
AustinJoo97 <austinjoo97@gmail.com>
2021-03-08 14:56:19 -0500

    added a function that will, upon clicking the 'next' button will change the currentKey to that which corresponds to the next question; upon reaching the last question, the currentKey will be reset to the intialKey; added a answerKey obj that will house each questions correct answer and will be used in some answerCheck function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
 let initialKey = "q1";
 let currentKey = initialKey;
+let finalKey;
+let chosenAnswer;
 let questions = {
     q1: "What kinds of data types can be used as a JS object's value?",
 
     q2: "How do you apply JS functionality on every paraphgraph element in an HTML file?",
 
-    q3: "What is the correct JavaScript syntax to change the content of the HTML element below? \n\n <p id='demo'>This is a demonstration.</p>",
+    q3: "What is the correct JavaScript syntax to change the content of the HTML element below? \r\n <p id='demo'>This is a demonstration.</p>",
 
     q4: "How would a developer alert the message 'Hello World!'?",
 
@@ -15,9 +17,9 @@ let answers = {
     q1: ["Arrays", "Numbers", "Strings", "All possible answers"],
 
     q2: [
-    "document.getElementbyID()", 
-    "document.querySelectorAll()", 
-    "document.paragraph", 
+    "document.getElementbyID('p')", 
+    "document.querySelectorAll('p')", 
+    "document.selectParagraph()", 
     "document.p.setAttribute()"],
 
     q3: [
@@ -40,11 +42,18 @@ let answers = {
         "let myFunc:function(){}"
     ]
 };
-let finalKey = Object.keys(questions).pop();
+let answerKey = {
+    q1: answers["q1"][3],
+    q2: answers["q2"][1],
+    q3: answers["q3"][0],
+    q4: answers["q4"][2],
+    q5: answers["q5"][2]
+}
+finalKey = Object.keys(questions).pop();
 
 
 let renderQuestion = function(){
-    document.getElementById("question").innerHTML = questions[currentKey];
+    document.getElementById("question").textContent = questions[currentKey];
 }
 
 let renderAnswers = function(){
@@ -56,7 +65,7 @@ let renderAnswers = function(){
     for(let i = 0; i < currentAnswerSet.length; i++){
         answerRendered = Math.floor(Math.random() * answersAvailable.length);
         if(answerRendered in answersAvailable){
-            document.getElementById(`answerChoice${answerToBeRendered}`).innerHTML = currentAnswerSet[answerRendered];
+            document.getElementById(`answerChoice${answerToBeRendered}`).textContent = currentAnswerSet[answerRendered];
             delete answersAvailable[answerRendered]
             answerToBeRendered++;
         } else {
@@ -81,12 +90,18 @@ let nextKeyAssigner = function(){
     }
 }
 
+// let chooseAnswer = function(event){
+//     console.log(`this is the event: ${JSON.stringify(event)}`);
+//     // chosenAnswer = event.value;
+// }
+
 let renderAll = function(){
+    nextKeyAssigner();
     renderQuestion();
     renderAnswers();
 }
 
-
+// document.getElementById("answerChoice1").addEventListener("click", chooseAnswer);
 document.getElementById("nextQuestion").addEventListener("click", nextKeyAssigner);
 document.getElementById("nextQuestion").addEventListener("click", renderAll);
 
