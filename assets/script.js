de2e36f2488000643eaf105137f7f83fb300c23a
AustinJoo97 <austinjoo97@gmail.com>
2021-03-13 19:59:00 -0500

    edited function 'chooseAnswer' to return non-chosen answer back to cyan

--- assets/script.js
+++ assets/script.js
@@ -137,7 +137,7 @@ let nextKeyAssigner = function(){
 // This function will retain the value of a user selected answer and use it to compare for a point
 let chooseAnswer = function(answer){
     for(let i = 1; i < 5; i++){
-        document.getElementById(`answerChoice${i}`).style.backgroundColor = 'cyan';
+        document.getElementById(`answerChoice${i}`).style.backgroundColor = "cyan";
     }
     answer.style.backgroundColor = 'greenyellow';
     chosenAnswer = answer.textContent;
